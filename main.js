61f6c937fcb56c6fc3fdb01f163c485c8f87e8b3
zombieaattak2009 <144689706+thenewcodingera2023@users.noreply.github.com>
2023-09-21 13:59:35 +0700

    New Slideshow
    Added New Slideshow Technique. Currently fixing bug on JS file.

--- main.js
+++ main.js
@@ -1,28 +1,24 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+let slideIndex = [1, 1, 1]; // Add an additional element for the third slide group
+let slideId = ["mySlides1", "mySlides2", "mySlides3", "mySlides4", "mySlides5"];
+showSlides(1, 0);
+showSlides(1, 1);
+showSlides(1, 2); // Call showSlides for the third slide group
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
 }
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
+function showSlides(n, no) {
   let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+  let x = document.getElementsByClassName(slideId[no]);
+  if (n > x.length) {
+    slideIndex[no] = 1;
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+  if (n < 1) {
+    slideIndex[no] = x.length;
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex[no] - 1].style.display = "block";
 }
